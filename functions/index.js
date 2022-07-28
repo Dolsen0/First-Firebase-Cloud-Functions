33a47650573a89d6f8dd15a865c8ae64567e4e89
Dolsen0 <derek.olsen@gmail.com>
2022-07-28 15:49:13 -0400

    update to firebase cloud

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,27 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+
+
+app.get("/test", (req, res) => {
+    res.send("Passed the test")
+});
+
+
+app.get("/another", (req, res) => {
+    res.send("It's still working")
+});
 
 exports.hello = functions.https.onRequest((req, res) =>{
     res.send('Im on firebase')
-}
-)
+});
+
+
+exports.api = functions.https.onRequest(app);
+
 
 
 
